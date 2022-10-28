2e12ff65458b96edcd41d08daacbf09a8e866f8d
jcarrion1978 <jorge.carrion.q@outlook.com>
2022-10-28 14:10:19 -0500

    Agregar archivos de proyecto.

---
+++ clsLibMate/Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace clsLibMate
+{
+    public class Numero
+    {
+        private int num1;
+        private int num2;
+
+        public Numero()
+        {
+        }
+
+        public int Num1 { get => num1; set => num1 = value; }
+        public int Num2 { get => num2; set => num2 = value; }
+
+        public int Suma()
+        {
+            return num1 + num2;
+        }
+    }
+}
